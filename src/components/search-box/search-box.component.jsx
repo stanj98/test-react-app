36faf8028df70468a48ee708ed668967fca642d0
Stanley John <infosoftallianze@gmail.com>
2021-02-20 17:26:06 +0530

    first react app

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './search-box.styles.css';
+
+export const SearchBox = (props) => (
+    
+    <input className="search" type="search" placeholder= {props.placeholder} onChange = {props.handleChange} />
+)
+ 
